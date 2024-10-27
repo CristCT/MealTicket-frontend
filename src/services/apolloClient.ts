5b65017859b7bb92edf17111e71feeeebd179476
ccuevas <ccuevas@previred.com>
2024-10-26 23:40:54 -0300

    add port config

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.VITE_API_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
